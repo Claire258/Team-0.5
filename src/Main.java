dbc64df789329eb759d2798a63f5524d8a0fac0c
Claire258 <112729820+Claire258@users.noreply.github.com>
2024-11-20 20:17:42 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,33 @@ import database.PlayerManager;
 
 public class Main {
     public static void main(String[] args) {
+		//System.out.println("Beginning");
         PlayerManager playerManager = new PlayerManager();
+        //System.out.println("Check?");
         SplashScreen splashScreen = new SplashScreen();
+        //System.out.println("Checkk??");
         PhotonServerSocket pss = new PhotonServerSocket();
-        PlayerEntryScreen playerEntryScreen = new PlayerEntryScreen();
+        //System.out.println("Checkeroni?");
+        PlayerEntryScreen playerEntryScreen = new PlayerEntryScreen(pss);
 
         splashScreen.display();
+        //System.out.println("1");
         playerEntryScreen.display();
-
+        //System.out.println("2");
+        //Thread serverThread = new Thread(() -> pss.start()); //added
+		//System.out.println("3");
         playerManager.loadPlayers();
+        //System.out.println("4");
+        Thread serverThread = new Thread(() -> {
+			try {
+				pss.run();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		});
+		serverThread.start();
 
 
     }
