391d123aa90be9949f1ef01f0b9acf106308c1bb
Claire258 <112729820+Claire258@users.noreply.github.com>
2024-11-22 13:13:27 -0600

    Update Main.java
    Updated main to account for UDP changes

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,26 @@
 import view.PlayerEntryScreen;
 import view.SplashScreen;
-import network.PhotonServerSocket;
+import network.PhotonClient;
 import database.PlayerManager;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-		//System.out.println("Beginning");
+	
+	try {
         PlayerManager playerManager = new PlayerManager();
-        //System.out.println("Check?");
+
         SplashScreen splashScreen = new SplashScreen();
-        //System.out.println("Checkk??");
-        PhotonServerSocket pss = new PhotonServerSocket();
-        //System.out.println("Checkeroni?");
+    
+        PhotonClient pss = new PhotonClient();
         PlayerEntryScreen playerEntryScreen = new PlayerEntryScreen(pss);
 
         splashScreen.display();
-        //System.out.println("1");
+
         playerEntryScreen.display();
-        //System.out.println("2");
-        //Thread serverThread = new Thread(() -> pss.start()); //added
-		//System.out.println("3");
+        
         playerManager.loadPlayers();
-        //System.out.println("4");
+ 
         Thread serverThread = new Thread(() -> {
 			try {
 				pss.run();
@@ -32,7 +31,8 @@ public class Main {
 			}
 		});
 		serverThread.start();
-
-
-    }
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	}
 }
